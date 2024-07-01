6da012031bc4ca7e5f8f10c55a2ec46efa720c8d
lera777 <eklera0506@gmail.com>
2024-07-01 09:30:10 +0300

    Delete tests/CaseSensitivity_Username.spec.js

--- tests/CaseSensitivity_Username.spec.js
+++
@@ -1,9 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-  test('Username case sensitivity', async ({ page }) => {
-    await page.goto('/login');
-    await page.fill('input[name="username"]', 'TOMsmith'); // Different case
-    await page.fill('input[name="password"]', 'SuperSecretPassword!');
-    await page.click('button:has-text("Login")');
-    await expect(page.locator('.error')).toContainText('Your username is invalid!');
-  });
